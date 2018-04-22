05b19bcd21d0a9fb7c350cce0d01eb2d8966927b
tianyu139 <liutianyu100@hotmail.com>
2018-04-22 20:06:12 +0800

    Amend comments

--- player.py
+++ player.py
@@ -1,11 +1,4 @@
-# uncompyle6 version 2.9.11
-# Python bytecode 3.5 (3350)
-# Decompiled from: Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
-# [GCC 5.4.0 20160609]
-# Embedded file name: /home/tianyu/Programming/liubot_telegram/player.py
-# Compiled at: 2017-05-12 17:44:26
-# Size of source mod 2**32: 135 bytes
-
+# Player Class
 
 class Player(object):
 
@@ -13,4 +6,3 @@ class Player(object):
         self.token = token
         self.name = name
         self.player_id = player_id
-# okay decompiling __pycache__/player.cpython-35.pyc
